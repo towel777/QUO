76ea456fff380083eefde8972a74b9385473c3f0
mGfPCEwG <toprup@mail.ru>
2022-02-16 14:36:52 +0300

    fix connect to Heroku Postgres services using SQLAlchemy >= 1.4.x

--- QUO/__init__.py
+++ QUO/__init__.py
@@ -6,9 +6,15 @@ from flask import Flask
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    # This will allow you to connect to Heroku Postgres services using SQLAlchemy >= 1.4.x
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY"),
-        SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL"),
+        SQLALCHEMY_DATABASE_URI=uri,
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
     from . import db
