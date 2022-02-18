f8468c26475c41c1d6652b32ed8be161a6c83847
Alexader Egorov <alexander.egorov@jetruby.com>
2022-02-18 17:37:17 +0300

    Create hook useInterval

---
+++ src/js/hooks/useInterval.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+export function useInterval(time, delay) {
+    // запомнить переданное время в секундах
+    const [timeLeft, setTimeLeft] = useState(time);
+
+    useEffect(() => {
+        // не запускать когда не задано время задержки
+        if (delay === null) return;
+
+        // уменьшать время на единицу
+        const tick = () => {
+            setTimeLeft(timeLeft - 1);
+        };
+
+        // старт
+        const timerId = setInterval(tick, delay);
+
+        // остановить если время истекло
+        if (timeLeft <= 0) clearInterval(timerId);
+
+        // очистить интервал
+        return () => clearInterval(timerId);
+    }, [delay, timeLeft]);
+
+    // передать управление интервалом вовне
+    return [timeLeft, setTimeLeft];
+}
