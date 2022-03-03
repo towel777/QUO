40262602ecf9fcc6c8f2f057502b15b9235644c9
Alexader Egorov <alexander.egorov@jetruby.com>
2022-03-03 20:59:56 +0300

    Create all request and styles

---
+++ src/js/features/Employee/dal/request.js
@@ -0,0 +1,77 @@
+
+const employeeArray =  [{
+    id: 2,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 1,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 4,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 5,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 6,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 7,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 8,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 9,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 10,
+    fullName: 'Grisha b',
+    position: 'Junior front end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+},{
+    id: 3,
+    fullName: 'Luyee f',
+    position: 'Middle back end developer',
+    email: 'mail.mailAmail.com',
+    pdp: 'PDP',
+    status: 'user'
+}
+]
+
+export const requestEmployee = () => {
+    return employeeArray
+}
