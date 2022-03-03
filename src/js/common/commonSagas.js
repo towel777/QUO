65fe307aeaea8dd750ef5837a23ca99ff0dcb2fb
Alexader Egorov <alexander.egorov@jetruby.com>
2022-03-03 20:57:25 +0300

    Create common files

--- src/js/common/commonSagas.js
+++ src/js/common/commonSagas.js
@@ -1,7 +1,21 @@
 import {all} from 'redux-saga/effects'
 import {testsWatcher} from "../features/Tests/core/sagas/testsSaga";
 import {createdTestsWatcher} from "../features/Tests/core/sagas/createdTestsSaga";
+import {tasksWatcher} from "../features/Tasks/core/sagas/tasksSaga";
+import {employeeWatcher} from "../features/Employee/sagas/employeeSaga";
+import {addEmployeeWatcher} from "../features/Menu/admin/core/sagas/addEmployeeSaga";
+import {resultTestsWatcher} from "../features/Tests/ResultTest/core/sagas/resultTestSaga";
+import {authenticationWatcher} from "../features/authentification/core/sagas/authentificationSaga";
+import {profileWatcher} from "../features/Profile/saga/profileSaga";
 
 export function * rootWatcher () {
-    yield all([testsWatcher(), createdTestsWatcher()])
+    yield all([
+        testsWatcher(),
+        createdTestsWatcher(),
+        tasksWatcher(),
+        employeeWatcher(),
+        addEmployeeWatcher(),
+        resultTestsWatcher(),
+        authenticationWatcher(),
+        profileWatcher()])
 }
---
+++ src/js/common/dal/request.js
@@ -0,0 +1,78 @@
+import axios from "axios"
+
+const testsArray = [
+    {
+        id: 1,
+        name: 'English',
+        level: 'Junior',
+        time: 120,
+        questions: [
+            {id: 1, number: 1, description: 'What is React', condition: 'waiting', answer: ''},
+            {id: 4, number: 4, description: 'What is Front-End', condition: 'waiting', answer: ''},
+            {id: 2, number: 2, description: 'What is Back-End', condition: 'waiting', answer: ''},
+            {id: 3, number: 3, description: 'Who are you', condition: 'waiting', answer: ''} ]
+    },
+    {
+        id: 3,
+        name: 'Back-End',
+        level: 'Junior',
+        time: 120,
+        questions: [
+            {id: 1, number: 1, description: 'What is React', condition: 'waiting', answer: ''},
+            {id: 4, number: 2, description: 'What is Front-End', condition: 'waiting', answer: ''},
+            {id: 2, number: 3, description: 'What is Back-End', condition: 'waiting', answer: ''},
+            {id: 4, number: 4, description: 'Who are you', condition: 'waiting', answer: ''} ]
+    },
+    {
+        id: 2,
+        name: 'Front-End',
+        level: 'Middle',
+        time: 160,
+        questions: [
+            {id: 1, number: 1, description: 'What is React', condition: 'waiting', answer: ''},
+            {id: 2, number: 2, description: 'What is Front-End', condition: 'waiting', answer: ''},
+            {id: 3, number: 3, description: 'What is Back-End', condition: 'waiting', answer: ''},
+            {id: 4, number: 4, description: 'Who are you', condition: 'waiting', answer: ''} ]
+    }
+]
+
+const instance = axios.create({
+    baseURL: 'https://quo-test.herokuapp.com/',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+
+})
+
+export const authentication = {
+    postLogin (email, psw) {
+        return instance.post(`auth/api/login`, {email, psw})
+            .then(response => response.data)
+    },
+    getProfile (session_token) {
+        return instance.post(`auth/api/profile`, {session_token})
+            .then(response => response.data)
+    },
+    postNewCompany (name, employee, positions) {
+        debugger
+        return instance.post(`auth/api/NewCompany`, {name, employee, positions})
+            .then(response => response.data)
+    }
+}
+
+export const users = {
+    postPosition (session_token) {
+        return instance.post(`auth/api/positions`, {session_token})
+            .then(response => response.data)
+    },
+    postNewEmployee (session_token, email, full_name, admin_status, position) {
+        debugger
+        return instance.post(`auth/api/NewEmployee`, {session_token, email, full_name, admin_status, position})
+            .then(response => response.data)
+    }
+}
+
+
+export const getResultTestsData = () => {
+    return testsArray
+}
